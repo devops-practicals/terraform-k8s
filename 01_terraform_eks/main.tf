330e756ee372268d5d17b33bdf5c65d072be9227
testuser <testuser@example.com>
2023-11-25 20:14:31 +0530

    add code

--- 01_terraform_eks/main.tf
+++ 01_terraform_eks/main.tf
@@ -80,7 +80,7 @@ module "eks" {
     one = {
       name = "node-group-1"
 
-      instance_types = ["t3.small"]
+      instance_types = ["t4g.small"]
 
       min_size     = 1
       max_size     = 2
