c1d092236466949a9b9e48d8537d3ddff0aa530f
testuser <testuser@example.com>
2023-11-25 20:51:10 +0530

    add code

--- 01_terraform_eks/main.tf
+++ 01_terraform_eks/main.tf
@@ -5,11 +5,11 @@ provider "aws" {
 data "aws_availability_zones" "available" {}
 
 data "aws_eks_cluster" "cluster" {
-  name = module.eks.cluster_id
+  name = module.eks.cluster_name
 }
 
 data "aws_eks_cluster_auth" "cluster" {
-  name = module.eks.cluster_name
+  name = module.eks.cluster_id
 }
 
 locals {
