6bfe77ab4b9a4b20df6d68cd10c3f59d3eae6e2a
testuser <testuser@example.com>
2023-11-25 22:39:02 +0530

    add code

--- 01_terraform_eks/main.tf
+++ 01_terraform_eks/main.tf
@@ -21,11 +21,11 @@ resource "random_string" "suffix" {
   special = false
 }
 
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
-}
+# provider "kubernetes" {
+#   host                   = data.aws_eks_cluster.cluster.endpoint
+#   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+#   token                  = data.aws_eks_cluster_auth.cluster.token
+# }
 
 # module "eks-kubeconfig" {
 #   source  = "hyperbadger/eks-kubeconfig/aws"
